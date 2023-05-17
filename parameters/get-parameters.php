a3cb7e0518d44e3d1753b72894fda192161325f0
Nabriz5855 <87419736+Nabriz5855@users.noreply.github.com>
2023-05-17 16:08:39 +1000

    Add files via upload

---
+++ parameters/get-parameters.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ 
+
+
+// ************ STUDENTS UPDATE ABOVE PARAMETER SECTION *********** //
+
+// *******************PARAMETER SECTION *************************** //
+
+ // ###############################======================> ADD  "star /" TO DISABLE SECTION 2 ( Remove "star /" in start of tis line )  <================================================#######################
+
+
+
+// Configuration for database connection
+
+# REMOTE DECLARATION
+$host       = getenv('nabin.mysql.database.azure.com');
+$username   = getenv('Nabin.Rijal1@studytafensw.edu.au');
+$password   = getenv('mnbvcxz1#');
+$db_name     = getenv('nabin');
+$sslcert    = 'ssl/DigiCertGlobalRootCA.crt.pem';
+$sslcert   = getenv('DB_SSLCERT');
+
+# LOCAL DECLARATION - for Testing only
+#$host       = 'SQLdbENDPOINT';
+#$username   = 'USERNAME';
+#$password   = 'PASSWORD';
+#$db_name    = 'DBNAME';
+#$sslcert    = 'ssl/DigiCertGlobalRootCA.crt.pem';
+
+
+# Local file creation - for testing only
+#$sslcertstring   = getenv('DB_SSLCERT');
+#$var_str = var_export($sslcertstring , true);
+#$testpemcontent = "$var_str";
+#file_put_contents('testcertsb.pem', $testpemcontent);
+#$sslcert    = "testcertsb.pem";
+?>
+
+ 
